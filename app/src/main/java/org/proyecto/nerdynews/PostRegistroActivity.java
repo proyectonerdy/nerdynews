aa8807143bc2ca4dd3e2426d841ab13cdefd88b9
Jose Manuel Castellano Dominguez <GPR_1985@hotmail.com>
2017-11-08 23:11:43 +0100

    Se inicializa PostRegistro

---
+++ app/src/main/java/org/proyecto/nerdynews/PostRegistroActivity.java
@@ -0,0 +1,13 @@
+package org.proyecto.nerdynews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostRegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_registro);
+    }
+}
