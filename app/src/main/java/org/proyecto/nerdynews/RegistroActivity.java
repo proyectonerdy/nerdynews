0f5a5a8ca31fc336e920212b2bb11734a8948791
Jose Manuel Castellano Dominguez <GPR_1985@hotmail.com>
2017-11-10 14:28:56 +0100

    Se implementa pantalla Registro

--- app/src/main/java/org/proyecto/nerdynews/RegistroActivity.java
+++ app/src/main/java/org/proyecto/nerdynews/RegistroActivity.java
@@ -2,6 +2,11 @@ package org.proyecto.nerdynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,4 +15,37 @@ public class RegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
+    public void resetField(View v){
+        if(v.getId() == R.id.crossemail){
+            AutoCompleteTextView ac = (AutoCompleteTextView) this.findViewById(R.id.email);
+            ac.setText("");
+        }
+        if(v.getId() == R.id.crosspassword){
+            EditText e = (EditText) this.findViewById(R.id.password);
+            e.setText("");
+        }
+        //si es otro se ignora
+    }
+
+    public void tooglePassword(View v){
+        EditText contraseña = (EditText) findViewById(R.id.password);
+        int tipo = contraseña.getInputType();
+        ImageButton ib = (ImageButton) findViewById(R.id.seepassword);
+        if (tipo == InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+            //hay que cambiar el icono del boton por el icono tachado
+            ib.setImageResource(R.drawable.eye_off);
+        }
+        else {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            //hay que cambiar el icono del boton por el icono no tachado
+            ib.setImageResource(R.drawable.eye);
+        }
+    }
+
+    public void irLogin(View v){
+        //TODO Esto se hara en la rama principal de Login/Registro
+    }
+
 }
