8d61d67ba6c5db536dccd5a8bd52e217dbfe637e
Jose Manuel Castellano Dominguez <GPR_1985@hotmail.com>
2017-12-02 18:22:07 +0100

    Rama Añadir Mi Calendario

--- app/src/main/java/org/proyecto/nerdynews/Utils/NavigationDrawerNavigate.java
+++ app/src/main/java/org/proyecto/nerdynews/Utils/NavigationDrawerNavigate.java
@@ -2,9 +2,12 @@ package org.proyecto.nerdynews.Utils;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.provider.CalendarContract;
 import android.support.v4.content.MimeTypeFilter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,6 +22,8 @@ import org.proyecto.nerdynews.intereses.ListadoFavoritosActivity;
 import org.proyecto.nerdynews.intereses.ListadoInteresesActivity;
 import org.proyecto.nerdynews.Perfil.PerfilActivity;
 
+import java.util.Date;
+
 /**
  * Created by jmcastellano on 13/11/2017.
  */
@@ -28,6 +33,7 @@ public class NavigationDrawerNavigate {
     public static boolean Navigate(MenuItem item, final Activity actividad){
         // Handle navigation view item clicks here
         DrawerLayout drawer = (DrawerLayout) actividad.findViewById(R.id.lidrawer_layout);
+        Intent intent = null;
         switch (item.getItemId()){
             case R.id.nav_amigos:
                 drawer.closeDrawer(GravityCompat.START);
@@ -53,11 +59,23 @@ public class NavigationDrawerNavigate {
                     actividad.startActivity(new Intent(actividad, ListadoFavoritosActivity.class), ActivityOptions.makeSceneTransitionAnimation(actividad).toBundle());
                 }
                 break;
+            case R.id.nav_micalendario:
+                drawer.closeDrawer(GravityCompat.START);
+                //hay que abrir el calendario de google asociado a la cuenta del cliente
+                Date fecha = new Date();
+                long startMillis = fecha.getTime();
+                Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+                builder.appendPath("time");
+                ContentUris.appendId(builder, startMillis);
+                intent = new Intent(Intent.ACTION_VIEW)
+                        .setData(builder.build());
+                actividad.startActivity(intent);
+                break;
             case R.id.nav_perfil:
                 //startActivity(new Intent(this, PerfilActivity.class));
                 drawer.closeDrawer(GravityCompat.START);
                 if(!actividad.getClass().getSimpleName().equals(ListadoFavoritosActivity.class.getSimpleName())) {
-                    Intent intent = new Intent(actividad, PerfilActivity.class);
+                    intent = new Intent(actividad, PerfilActivity.class);
                     intent.putExtra("registro", "N");
                     actividad.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(actividad).toBundle());
                 }
@@ -69,7 +87,7 @@ public class NavigationDrawerNavigate {
                 editor.commit();
 
                 drawer.closeDrawer(GravityCompat.START);
-                Intent intent = new Intent(actividad, LoginActivity.class);
+                intent = new Intent(actividad, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 actividad.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(actividad).toBundle());
                 Thread t = new Thread(new Runnable() {
